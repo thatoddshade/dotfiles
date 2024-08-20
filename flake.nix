51beed41436c243bc981181d264fbeeb3c87a6a0
thatoddshade <thatoddshade+git@proton.me>
2024-08-20 18:57:16 +0200

    style: remove commented nix-color input line

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-		#nix-colors.url = "github:misterio77/nix-colors";
 		stylix.url = "github:danth/stylix";
 
 		#systems.url = "github:nix-systems/default";
