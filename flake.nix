c0ab8ce050b2712c027aa047ab630e3db946779f
thatoddshade <thatoddshade@proton.me>
2026-07-07 20:17:03 +0200

    Update nixpkgs input versions

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
 	description = "thatoddshade's NIXOS, HOME MANAGER and NIX-ON-DROID flake configuration";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
 		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
 
 		home-manager = {
-			url = "github:nix-community/home-manager/release-24.11";
+			url = "github:nix-community/home-manager/release-26.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
