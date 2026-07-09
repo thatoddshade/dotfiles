2f12f0f9489e69e64ca6b2ef95c5be632a51b135
thatoddshade <thatoddshade@gmx.com>
2026-07-10 01:23:41 +0200

    Remove development-related packages from NixOS

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -1,25 +1,17 @@
 { inputs, pkgs, ... }:
 {
 	environment.systemPackages = with pkgs; [
-		asciidoctor
 		asciinema
 		asciinema-agg
 		borgbackup
-		#clang
 		curl
-		cmark
 		dysk
 		evil-helix
 		fastfetch
 		ffmpeg
 		fzf
-		#gcc
-		ghc # Haskell compiler
 		haskell-language-server # HSL
-		stack # Alternative to cabal, Haskell build tool
-		#haskellPackages.ghcup	# GHC toolchain installer for Haskell development		# one of its dependencies is for some reason marked as broken…
 		gh
-		#gnumake
 		hello
 		#inputs.dmm.packages.x86_64-linux.default
 		imagemagick
@@ -29,14 +21,8 @@
 		mprocs
 		#newsboat
 		nvd	# Diff tool for Nix profiles
-		pandoc
-		python3
 		plan9port
-		rustup
 		ripgrep	# Grep Rust rewrite
-		dart-sass
-		soupault
-		haskellPackages.stack
 		starship
 		tmux
 		trash-cli
@@ -46,7 +32,6 @@
 		wget
 		wiki-tui
 		yt-dlp
-		zola	# Markdown and TOML frontmatter SSG written in Rust
 		zellij
 		zoxide
 
