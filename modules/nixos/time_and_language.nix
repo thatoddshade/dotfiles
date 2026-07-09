e3b74cb0727c5856ca449b4431b578eceda6c0ca
thatoddshade <thatoddshade@gmx.com>
2026-07-09 02:20:08 +0200

    Configure i18n.extraLocaleSettings

--- modules/nixos/time_and_language.nix
+++ modules/nixos/time_and_language.nix
@@ -2,6 +2,17 @@
 {
 	time.timeZone = "Europe/Paris";
 	i18n.defaultLocale = "fr_FR.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "fr_FR.UTF-8";
+		LC_IDENTIFICATION = "fr_FR.UTF-8";
+		LC_MEASUREMENT = "fr_FR.UTF-8";
+		LC_MONETARY = "fr_FR.UTF-8";
+		LC_NAME = "fr_FR.UTF-8";
+		LC_NUMERIC = "fr_FR.UTF-8";
+		LC_PAPER = "fr_FR.UTF-8";
+		LC_TELEPHONE = "fr_FR.UTF-8";
+		LC_TIME = "fr_FR.UTF-8";
+	};
 
 	# set the keyboard layout to EurKEY
 	services.xserver.xkb.layout = "eu";
